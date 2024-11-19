6c7474841770b4a67ef30426ee57818c335dce98
Edward A Maxedon <edadma@gmail.com>
2024-11-19 12:19:37 -0500

    update

--- build.sbt
+++ build.sbt
@@ -1,41 +1,45 @@
 ThisBuild / licenses += "ISC" -> url("https://opensource.org/licenses/ISC")
-ThisBuild / versionScheme := Some("semver-spec")
+ThisBuild / versionScheme     := Some("semver-spec")
 
 publish / skip := true
 
-lazy val csv = crossProject(JSPlatform, JVMPlatform, NativePlatform).in(file(".")).
-  settings(
-    name := "csv",
-    version := "0.1.4",
-    scalaVersion := "3.1.3",
+lazy val csv = crossProject(JSPlatform, JVMPlatform, NativePlatform)
+  .in(file("."))
+  .settings(
+    name         := "csv",
+    version      := "0.1.5",
+    scalaVersion := "3.5.2",
     scalacOptions ++=
       Seq(
-        "-deprecation", "-feature", "-unchecked",
-        "-language:postfixOps", "-language:implicitConversions", "-language:existentials", "-language:dynamics"
+        "-deprecation",
+        "-feature",
+        "-unchecked",
+        "-language:postfixOps",
+        "-language:implicitConversions",
+        "-language:existentials",
+        "-language:dynamics",
       ),
-    organization := "io.github.edadma",
-    githubOwner := "edadma",
-    githubRepository := name.value,
-    mainClass := Some(s"${organization.value}.${name.value}.Main"),
-    libraryDependencies += "org.scalatest" %%% "scalatest" % "3.2.12" % "test",
-    libraryDependencies += "io.github.edadma" %%% "char-reader" % "0.1.10",
-    libraryDependencies += "io.github.edadma" %%% "cross-platform" % "0.1.3",
-    publishMavenStyle := true,
-    Test / publishArtifact := false,
-    licenses += "ISC" -> url("https://opensource.org/licenses/ISC"),
-  ).
-  jvmSettings(
+    organization                               := "io.github.edadma",
+    githubOwner                                := "edadma",
+    githubRepository                           := name.value,
+    libraryDependencies += "org.scalatest"    %%% "scalatest"      % "3.2.19" % "test",
+    libraryDependencies += "io.github.edadma" %%% "char-reader"    % "0.1.16",
+    libraryDependencies += "io.github.edadma" %%% "cross-platform" % "0.0.3",
+    publishMavenStyle                          := true,
+    Test / publishArtifact                     := false,
+    licenses += "ISC"                          -> url("https://opensource.org/licenses/ISC"),
+  )
+  .jvmSettings(
     libraryDependencies += "org.scala-js" %% "scalajs-stubs" % "1.1.0" % "provided",
-  ).
-  nativeSettings(
-    nativeLinkStubs := true
-  ).
-  jsSettings(
+  )
+  .nativeSettings(
+  )
+  .jsSettings(
     scalaJSLinkerConfig ~= { _.withModuleKind(ModuleKind.CommonJSModule) },
     jsEnv := new org.scalajs.jsenv.nodejs.NodeJSEnv(),
     //    Test / scalaJSUseMainModuleInitializer := true,
     //    Test / scalaJSUseTestModuleInitializer := false,
     Test / scalaJSUseMainModuleInitializer := false,
     Test / scalaJSUseTestModuleInitializer := true,
-    scalaJSUseMainModuleInitializer := true,
+    scalaJSUseMainModuleInitializer        := true,
   )
--- shared/src/test/scala/io/github/edadma/csv/CSVReadTests.scala
+++ shared/src/test/scala/io/github/edadma/csv/CSVReadTests.scala
@@ -9,100 +9,111 @@ class CSVReadTests extends AnyFreeSpec with Matchers {
 
   "tab as delimiter" in {
     CSVRead.fromString("a\tb", '\t') shouldBe Success(List(List("a", "b")))
-//    assert(CSVRead.fromString("\tb", '\t') == Success(List(List("", "b"))))
-//    assert(CSVRead.fromString("a\t", '\t') == Success(List(List("a", ""))))
-//    assert(CSVRead.fromString("\t", '\t') == Success(List(List("", ""))))
+    assert(CSVRead.fromString("\tb", '\t') == Success(List(List("", "b"))))
+    assert(CSVRead.fromString("a\t", '\t') == Success(List(List("a", ""))))
+    assert(CSVRead.fromString("\t", '\t') == Success(List(List("", ""))))
   }
 
-//  "empty file" in {
-//    assert(CSVRead.fromString("") == Success(List(List(""))))
-//    assert(CSVRead.fromString("\n") == Success(List(List(""))))
-//    assert(CSVRead.fromString("\r\n") == Success(List(List(""))))
-//  }
-//
-//  "basic" in {
-//    assert(
-//      CSVRead.fromString(
-//        """
-//          |11,12
-//          |21,22
-//          """.trim.stripMargin
-//      ) == Success(List(List("11", "12"), List("21", "22"))))
-//    assert(
-//      CSVRead.fromString(
-//        """
-//            |11
-//            |21
-//          """.trim.stripMargin
-//      ) == Success(List(List("11"), List("21"))))
-//    assert(
-//      CSVRead.fromString(
-//        """
-//            |11,12
-//          """.trim.stripMargin
-//      ) == Success(List(List("11", "12"))))
-//    assert(
-//      CSVRead.fromString(
-//        """
-//            |11
-//          """.trim.stripMargin
-//      ) == Success(List(List("11"))))
-//  }
-//
-//  "quoted fields" in {
-//    assert(
-//      CSVRead.fromString(
-//        """
-//            |11,"12"
-//            |"2""1","22"
-//          """.trim.stripMargin
-//      ) == Success(List(List("11", "12"), List("2\"1", "22"))))
-//  }
-//
-//  "empty fields" in {
-//    assert(
-//      CSVRead.fromString(
-//        """
-//            |,12
-//            |21,22
-//          """.trim.stripMargin
-//      ) == Success(List(List("", "12"), List("21", "22"))))
-//    assert(
-//      CSVRead.fromString(
-//        """
-//            |11,
-//            |21,22
-//          """.trim.stripMargin
-//      ) == Success(List(List("11", ""), List("21", "22"))))
-//    assert(
-//      CSVRead.fromString(
-//        """
-//            |11,12
-//            |,22
-//          """.trim.stripMargin
-//      ) == Success(List(List("11", "12"), List("", "22"))))
-//    assert(
-//      CSVRead.fromString(
-//        """
-//            |11,12
-//            |21,
-//          """.trim.stripMargin
-//      ) == Success(List(List("11", "12"), List("21", ""))))
-//    assert(
-//      CSVRead.fromString(
-//        """
-//            |
-//            |21
-//          """.trim.stripMargin
-//      ) == Success(List(List(""), List("21"))))
-//    assert(
-//      CSVRead.fromString(
-//        """
-//            |11
-//            |
-//            |
-//          """.trim.stripMargin
-//      ) == Success(List(List("11"), List(""))))
-//  }
+  "empty file" in {
+    assert(CSVRead.fromString("") == Success(List(List(""))))
+    assert(CSVRead.fromString("\n") == Success(List(List(""))))
+    assert(CSVRead.fromString("\r\n") == Success(List(List(""))))
+  }
+
+  "basic" in {
+    assert(
+      CSVRead.fromString(
+        """
+          |11,12
+          |21,22
+          """.trim.stripMargin,
+      ) == Success(List(List("11", "12"), List("21", "22"))),
+    )
+    assert(
+      CSVRead.fromString(
+        """
+            |11
+            |21
+          """.trim.stripMargin,
+      ) == Success(List(List("11"), List("21"))),
+    )
+    assert(
+      CSVRead.fromString(
+        """
+            |11,12
+          """.trim.stripMargin,
+      ) == Success(List(List("11", "12"))),
+    )
+    assert(
+      CSVRead.fromString(
+        """
+            |11
+          """.trim.stripMargin,
+      ) == Success(List(List("11"))),
+    )
+  }
+
+  "quoted fields" in {
+    assert(
+      CSVRead.fromString(
+        """
+            |11,"12"
+            |"2""1","22"
+          """.trim.stripMargin,
+      ) == Success(List(List("11", "12"), List("2\"1", "22"))),
+    )
+  }
+
+  "empty fields" in {
+    assert(
+      CSVRead.fromString(
+        """
+            |,12
+            |21,22
+          """.trim.stripMargin,
+      ) == Success(List(List("", "12"), List("21", "22"))),
+    )
+    assert(
+      CSVRead.fromString(
+        """
+            |11,
+            |21,22
+          """.trim.stripMargin,
+      ) == Success(List(List("11", ""), List("21", "22"))),
+    )
+    assert(
+      CSVRead.fromString(
+        """
+            |11,12
+            |,22
+          """.trim.stripMargin,
+      ) == Success(List(List("11", "12"), List("", "22"))),
+    )
+    assert(
+      CSVRead.fromString(
+        """
+            |11,12
+            |21,
+          """.trim.stripMargin,
+      ) == Success(List(List("11", "12"), List("21", ""))),
+    )
+    assert(
+      CSVRead.fromString(
+        """
+            |
+            |21
+          """.trim.stripMargin,
+      ) == Success(List(List(""), List("21"))),
+    )
+    assert(
+      CSVRead.fromString(
+        """
+            |11
+            |
+            |
+          """.trim.stripMargin,
+      ) == Success(List(List("11"), List(""))),
+    )
+  }
 
 }
