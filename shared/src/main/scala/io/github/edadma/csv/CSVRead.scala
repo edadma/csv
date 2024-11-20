750f6710715c577924c92a20a4675c07259962f4
Edward A Maxedon <edadma@gmail.com>
2024-11-20 10:18:15 -0500

    streaming

--- shared/src/main/scala/io/github/edadma/csv/CSVRead.scala
+++ shared/src/main/scala/io/github/edadma/csv/CSVRead.scala
@@ -9,20 +9,29 @@ import scala.annotation.tailrec
 
 object CSVRead {
 
-  def fromFile(file: String, delimiter: Char = ','): Try[List[List[String]]] = {
+  def fromFile(file: String, delimiter: Char = ','): Try[Seq[Seq[String]]] = {
     require(readableFile(file), s"not readable: $file")
 
     fromReader(CharReader.fromFile(file), delimiter)
   }
 
-  def fromString(s: String, delimiter: Char = ','): Try[List[List[String]]] = fromReader(CharReader.fromString(s), delimiter)
+  def fromString(s: String, delimiter: Char = ','): Try[Seq[Seq[String]]] =
+    fromReader(CharReader.fromString(s), delimiter)
 
-  def fromReader(in: CharReader, delimiter: Char = ','): Try[List[List[String]]] = {
-    var input   = in
-    val records = new ListBuffer[List[String]]
+  def fromReader(in: CharReader, delimiter: Char = ','): Try[Seq[Seq[String]]] = {
+    val records = new ListBuffer[Seq[String]]
+
+    Try {
+      fromReaderStreamed(in, records += _, delimiter)
+      records.toSeq
+    }
+  }
+
+  def fromReaderStreamed(in: CharReader, output: Seq[String] => Unit, delimiter: Char = ','): Unit =
+    var input = in
 
     @tailrec
-    def readInput: List[List[String]] = {
+    def readInput(): Unit = {
       def consume(except: Char*): String = {
         val buf = new StringBuilder
 
@@ -69,17 +78,17 @@ object CSVRead {
         val l = new ListBuffer[String]
 
         @tailrec
-        def fields: List[String] = {
+        def fields: Seq[String] = {
           l += field
 
           if (input.more && input.ch == delimiter) {
             advance()
             fields
           } else
-            l.toList
+            l.toSeq
         }
 
-        records += fields
+        output(fields)
 
         opt('\r')
 
@@ -113,13 +122,116 @@ object CSVRead {
 
       if (input.more) {
         record()
-        readInput
-      } else if (records.isEmpty)
-        List(List("")) // according to spec a csv file has at least one record
-      else
-        records.toList
+        readInput()
+      }
     }
 
-    Try(readInput)
-  }
+    readInput()
+  end fromReaderStreamed
+
+//  def fromReader(in: CharReader, delimiter: Char = ','): Try[List[List[String]]] = {
+//    var input   = in
+//    val records = new ListBuffer[List[String]]
+//
+//    @tailrec
+//    def readInput: List[List[String]] = {
+//      def consume(except: Char*): String = {
+//        val buf = new StringBuilder
+//
+//        @tailrec
+//        def consume: String =
+//          if (input.eoi || (except contains input.ch))
+//            buf.toString
+//          else {
+//            buf += input.ch
+//            advance()
+//            consume
+//          }
+//
+//        consume
+//      }
+//
+//      def field =
+//        if (input.more) {
+//          if (input.ch == '"') {
+//            advance()
+//
+//            val chunks = new StringBuilder
+//
+//            @tailrec
+//            def chunk(): Unit = {
+//              chunks ++= consume('"')
+//
+//              if (input.next.more && input.next.ch == '"') {
+//                input = input.next.next
+//                chunks += '"'
+//                chunk()
+//              }
+//            }
+//
+//            chunk()
+//            chr('"')
+//            chunks.toString
+//          } else
+//            consume(delimiter, '\r', '\n', '"')
+//        } else
+//          ""
+//
+//      def record(): Unit = {
+//        val l = new ListBuffer[String]
+//
+//        @tailrec
+//        def fields: List[String] = {
+//          l += field
+//
+//          if (input.more && input.ch == delimiter) {
+//            advance()
+//            fields
+//          } else
+//            l.toList
+//        }
+//
+//        records += fields
+//
+//        opt('\r')
+//
+//        if (input.more)
+//          chr('\n')
+//      }
+//
+//      def advance(): Unit = input = input.next
+//
+//      def chr(c: Char): Unit = {
+//        val s =
+//          c match {
+//            case '\r' => "\\r"
+//            case '\n' => "\\n"
+//            case _    => s"$c"
+//          }
+//
+//        if (input.eoi)
+//          input.error(s"expected '$s', but end of input")
+//        else if (input.ch == c)
+//          advance()
+//        else
+//          input.error(s"expected '$s', but encountered '${input.ch}'")
+//      }
+//
+//      def opt(c: Char): Unit =
+//        if (input.more) {
+//          if (input.ch == c)
+//            chr(c)
+//        }
+//
+//      if (input.more) {
+//        record()
+//        readInput
+//      } else if (records.isEmpty)
+//        List(List("")) // according to spec a csv file has at least one record
+//      else
+//        records.toList
+//    }
+//
+//    Try(readInput)
+//  }
 }
